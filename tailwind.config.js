a7f48c95bc294e5b2e9e7903449665356b9db354
Ricky Valentino Tarigan <rickyvtarigan@gmail.com>
2023-11-21 15:33:49 +0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	theme: {
+		extend: {
+			colors: {
+				blue: "#10218B",
+				secondary: "#ddd",
+			},
+			backgroundImage: {
+				"card-image": "url('/src/images/heder.jpg')",
+				"bg-galeri1": "url('/src/images/galeri_sample1.jpg')",
+				"bg-galeri2": "url('/src/images/galeri_sample2.jpg')",
+			},
+		},
+	},
+	plugins: [],
+};
